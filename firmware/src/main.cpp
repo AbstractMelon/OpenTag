db7c5958d258dc2155347bbae2ba9edf0ab8a54f
Abstractmelon <abstractmelons@gmail.com>
2026-05-05 15:51:37 -0600

    Add PlatformIO project files

---
+++ firmware/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("ESP32 working");
+}
+
+void loop() {
+    delay(1000);
+    Serial.println("loop");
+}
